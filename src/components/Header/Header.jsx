e8e8e52991a876032b9c53b0f45b373feebde63e
Gleb Orlovtsev <the.unknown.guest@yandex.com>
2023-12-05 18:50:41 +0300

    Добавлен основной функционал, отредактирована верстка

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,8 +4,12 @@ import logo from '../../images/logo.svg';
 export default function Header() {
     return (
         <header className='header'>
-            <a className='header__logo' href='https://prosept.ru/' target='_blank' rel='noreferrer'><img alt='Логотип' src={logo} /></a>
-            <button type='button' className='header__button'>Выпадающее меню</button>
+            <div className='header__container'>
+                <a className='header__logo' href='https://prosept.ru/' target='_blank' rel='noreferrer'>
+                    <img className='header__img' alt='Логотип' src={logo} />
+                </a>
+                <button type='button' className='header__button'>Выпадающее меню</button>
+            </div>
         </header>
     )
 }
--- src/components/ProducerCard/ProducerCard.jsx
+++ src/components/ProducerCard/ProducerCard.jsx
@@ -1,11 +1,18 @@
+import { useSelector } from 'react-redux';
 import './ProducerCard.scss';
 
-export default function ProducerCard() {
+export default function ProducerCard({ product, handleChooseCard, proseptId, setProseptId }) {
+
+    const handleReset = (e) => {
+        e.stopPropagation();
+        setProseptId('');
+    }
     return (
-        <button className='producer-card'>
-            <p className='producer-card__item'>Артикул товара</p>
-            <p className='producer-card__item'>Название товара</p>
-            <p className='producer-card__item'>Цена товара</p>
-        </button>
+        <li className={`producer-card${+proseptId === product.id ? ' producer-card_chosen' : ''}`} id={product.id} onClick={handleChooseCard}>
+            <p className='producer-card__item'>{product.cost}</p>
+            <p className='producer-card__item producer-card__item_name'>{product.name_1c}</p>
+            <p className='producer-card__item'>{product.article} руб.</p>
+            <button className='producer-card__btn' onClick={handleReset}>✖</button>
+        </li>
     )
-}
+}   
